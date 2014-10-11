f553cfcc7faa5bd638ca502cfbd5da4a83bea33c
Kokal <slim_shady121.2@abv.bg>
2014-10-11 19:27:26 +0300

    Add is_an_bn

---
+++ D2/is_an_bn.py
@@ -0,0 +1,20 @@
+def is_an_bn(word):
+    count_a = 0
+    count_b = 0
+    for index, char in enumerate(word):
+        if char == 'a':
+            count_a += 1
+        elif char == 'b' and index != len(word) - 1 and word[index + 1] == 'a':
+            return False
+        elif char == 'b' and (index != len(word) - 1 and word[index + 1] == 'b') or index == len(word) - 1:
+            count_b += 1
+    if count_a == count_b:
+        print("n = ", count_a)
+        return True
+    else:
+        print(("a = ", count_a))
+        print(("b = ", count_b))
+        return False
+
+
+print(is_an_bn("aaabba"))
